cce4c5059bf05ea804c7b153384e7a6282feadc5
zhangcl <zhangchclo@sina.cn>
2016-04-14 23:18:31 +0800

    modify pom.xml

--- src/main/java/com/hunter/entity/MySqlColumnImpl.java
+++ src/main/java/com/hunter/entity/MySqlColumnImpl.java
@@ -2,9 +2,60 @@
 package com.hunter.entity;
 
 /**
- * @author zhangcl
+ * @author zhangcl 2016年4月14日 上午21:44:00
  *
  */
 public class MySqlColumnImpl implements DBColumnInte {
+    // int,tinyint,mediumint,int/integer,bigint
+    private String mysInt;
+    // float
+    private String mysFloat;
+    // double,decimal
+    private String mysDouble;
+    // char,varchar,tinyblob,tinytext,blob,text,mediumblob,
+    // mediumtext,longblob,longtext,binary,varbinary
+    private String mysChar;
+    // date,time,datetime,timestamp
+    private String myDate;
+
+    public String getMysInt() {
+	return mysInt;
+    }
+
+    public void setMysInt(String mysInt) {
+	this.mysInt = mysInt;
+    }
+
+    public String getMysFloat() {
+	return mysFloat;
+    }
+
+    public void setMysFloat(String mysFloat) {
+	this.mysFloat = mysFloat;
+    }
+
+    public String getMysDouble() {
+	return mysDouble;
+    }
+
+    public void setMysDouble(String mysDouble) {
+	this.mysDouble = mysDouble;
+    }
+
+    public String getMysChar() {
+	return mysChar;
+    }
+
+    public void setMysChar(String mysChar) {
+	this.mysChar = mysChar;
+    }
+
+    public String getMyDate() {
+	return myDate;
+    }
+
+    public void setMyDate(String myDate) {
+	this.myDate = myDate;
+    }
 
 }
--- src/main/java/com/hunter/entity/OracleColumnImpl.java
+++ src/main/java/com/hunter/entity/OracleColumnImpl.java
@@ -5,5 +5,44 @@ package com.hunter.entity;
  *
  */
 public class OracleColumnImpl implements DBColumnInte {
+    // char类型
+    private String oraChar;
+    // Date/timestamp
+    private String oraDate;
+    // number(n)
+    private String oraNumber1;
+    // number(n,n)
+    private String oraNumber2;
 
+    public String getOraChar() {
+	return oraChar;
+    }
+
+    public void setOraChar(String oraChar) {
+	this.oraChar = oraChar;
+    }
+
+    public String getOraDate() {
+	return oraDate;
+    }
+
+    public void setOraDate(String oraDate) {
+	this.oraDate = oraDate;
+    }
+
+    public String getOraNumber1() {
+	return oraNumber1;
+    }
+
+    public void setOraNumber1(String oraNumber1) {
+	this.oraNumber1 = oraNumber1;
+    }
+
+    public String getOraNumber2() {
+	return oraNumber2;
+    }
+
+    public void setOraNumber2(String oraNumber2) {
+	this.oraNumber2 = oraNumber2;
+    }
 }
