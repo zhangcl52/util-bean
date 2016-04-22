b08d93b56462caee26d4e35d01fb7d53765fa871
zhangcl52 <zhangcl52@ziroom.com>
2016-04-22 08:36:32 +0800

    修改excel读取类

--- src/main/java/com/hunter/util/POIReader.java
+++ src/main/java/com/hunter/util/POIReader.java
@@ -17,9 +17,22 @@ import java.util.List;
 public class POIReader implements ExcelReader {
     private Workbook wb = null;
 
-    public POIReader(String path) {
+    public POIReader(String filepath) {
+        init(filepath);
+    }
+
+    public POIReader() {
+
+    }
+
+    /**
+     * 初始化Workbook
+     *
+     * @param filepath
+     */
+    private void init(String filepath) {
         try {
-            wb = WorkbookFactory.getInstance(path);
+            wb = WorkbookFactory.getWorkbook(filepath);
         } catch (TypeNotMatchException e) {
             System.out.println(e.getMessage());
         } catch (Exception e) {
@@ -29,24 +42,41 @@ public class POIReader implements ExcelReader {
 
     @Override
     public List<ExcelSheet> readExcel(String filepath) {
+        if (this.wb == null) {
+            init(filepath);
+        }
+        return readExcel();
+    }
+
+    /**
+     * 读取excel
+     * @return
+     */
+    public List<ExcelSheet> readExcel() {
+        if (this.wb == null) {
+            return null;
+        }
         int sheetsCount = wb.getNumberOfSheets();
-        List<ExcelSheet> excelSheetList= Lists.newArrayList();
+        List<ExcelSheet> excelSheetList = Lists.newArrayList();
+        //循环sheet
         for (int i = 0; i < sheetsCount; i++) {
             Sheet sheet = wb.getSheetAt(i);
             ExcelSheet excelSheet = new ExcelSheet();
             excelSheet.setBeanName(sheet.getRow(0).getCell(0).getStringCellValue());
             List<ExcelRow> excelRows = Lists.newArrayList();
             int rowsCount = sheet.getPhysicalNumberOfRows();
+            //循环行
             for (int j = 0; j < rowsCount; j++) {
                 Row row = sheet.getRow(j);
                 int rowCounts = row.getPhysicalNumberOfCells();
-                ExcelRow excelRow=new ExcelRow();
-                List<Cell> cellList=Lists.newArrayList();
+                ExcelRow excelRow = new ExcelRow();
+                List<Cell> cellList = Lists.newArrayList();
+                //循环列
                 for (int k = 0; k < rowCounts; k++) {
                     Cell cell = new Cell();
                     cell.setColumn(k);
                     cell.setRow(j);
-                    cell.setContents(row.getCell(k).getStringCellValue());
+                    cell.setContents(getValueOfString(row.getCell(k)));
                     cellList.add(cell);
                 }
                 excelRow.setCells(cellList);
@@ -58,4 +88,26 @@ public class POIReader implements ExcelReader {
         return excelSheetList;
     }
 
+    /**
+     *  将excel中的各种类型的值转换为string类型
+     * @param cell
+     * @return String
+     */
+    public String getValueOfString(org.apache.poi.ss.usermodel.Cell cell){
+        String value=null;
+        if(cell.getCellType()==cell.CELL_TYPE_STRING){
+            value=cell.getStringCellValue();
+        }else if(cell.getCellType()==cell.CELL_TYPE_BLANK){
+            value="";
+        }else if(cell.getCellType()==cell.CELL_TYPE_BOOLEAN){
+            value=cell.getBooleanCellValue()?"true":"false";
+        }else if(cell.getCellType()==cell.CELL_TYPE_ERROR){
+            value="error";
+        }else if(cell.getCellType()==cell.CELL_TYPE_FORMULA){
+            value=cell.getCellFormula();
+        }else if(cell.getCellType()==cell.CELL_TYPE_NUMERIC){
+            value= cell.getNumericCellValue()+"";
+        }
+        return value;
+    }
 }
--- src/main/java/com/hunter/util/WorkbookFactory.java
+++ src/main/java/com/hunter/util/WorkbookFactory.java
@@ -12,32 +12,33 @@ import java.io.FileInputStream;
  * Created by zhangcl on 2016/4/19.
  */
 public class WorkbookFactory {
-    private static volatile Workbook wb = null;
+    private static Workbook wb = null;
     private static String XLS = "xls";
     private static String XLSX = "xlsx";
 
     private WorkbookFactory() {
     }
 
-    public static Workbook getInstance(String filePath) throws Exception{
+    /**
+     * 以xls结尾的文件用的是HSSFWorkbook类读取
+     * 以xlsx结尾的文件用的是XSSFWorkbook类读取
+     * @param filePath
+     * @return
+     * @throws Exception
+     */
+    public static Workbook getWorkbook(String filePath) throws Exception {
         if (Strings.isNullOrEmpty(filePath)) {
             return null;
         }
-        if(!filePath.endsWith(XLS)&&!filePath.endsWith(XLSX)){
+        if (!filePath.endsWith(XLS) && !filePath.endsWith(XLSX)) {
             throw new TypeNotMatchException("文件类型只能是.xlx或.xlsx");
         }
-        if(wb==null){
-            synchronized (Workbook.class){
-                if(wb==null){
-                    //HSSFWorkbook相当于一个excel文件，HSSFWorkbook是解析excel2007之前的版本（xls）
-                    //之后版本使用XSSFWorkbook（xlsx）
-                    if(filePath.endsWith(XLS)){
-                        wb=new HSSFWorkbook(new FileInputStream(filePath));
-                    }else if(filePath.endsWith(XLSX)){
-                        wb=new XSSFWorkbook(new FileInputStream(filePath));
-                    }
-                }
-            }
+        //HSSFWorkbook相当于一个excel文件，HSSFWorkbook是解析excel2007之前的版本（xls）
+        //之后版本使用XSSFWorkbook（xlsx）
+        if (filePath.endsWith(XLS)) {
+            wb = new HSSFWorkbook(new FileInputStream(filePath));
+        } else if (filePath.endsWith(XLSX)) {
+            wb = new XSSFWorkbook(new FileInputStream(filePath));
         }
         return wb;
     }
